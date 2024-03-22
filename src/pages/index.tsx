f9cb343d349dc9d496aaccdcc9ebcceda272eb57
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2024-03-22 15:46:15 +0900

    Add links for sign up, login, and home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <p>hello world</p>
+      <Link href="/signup">Sign up</Link>
+      <br />
+      <Link href="/login">Login</Link>
+      <br />
+      <Link href="/home">Home</Link>
     </>
   );
 }
